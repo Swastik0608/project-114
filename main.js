04ca4601bdc94fb46ea2c8a5467d2f67b442e9cb
Swastik0608 <102349946+Swastik0608@users.noreply.github.com>
2024-03-28 23:19:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+    
+}
+
+function modelLoaded()
+{
+
+}
+
+function gotPoses(results){
+
+}
+
+
+function draw()
+{
+    image(video, 0, 0, 300, 300)
+
+}
+
+function take_snapshot()
+{
+   
+}
